520fae45fb7677d941f8c3e824a3664785198ba6
shwetal <shwetalgadge@outlook.com>
2024-07-01 12:46:22 +0530

    code

--- src/test/java/com/base/Baseclass.java
+++ src/test/java/com/base/Baseclass.java
@@ -8,14 +8,12 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Baseclass {
 
-	protected AndroidDriver<MobileElement> driver;
-
+    protected AndroidDriver<MobileElement> driver;
 
     @BeforeTest
     public void setUp() {
@@ -24,21 +22,20 @@ public class Baseclass {
         caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Redmi Note 10S");
         caps.setCapability(MobileCapabilityType.UDID, "TCG6XCUG95X8DI7H");
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13 TP1A.220624.014");
-        caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chromium");
+        caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
 
         try {
             URL url = new URL("http://127.0.0.1:4723/wd/hub");
             driver = new AndroidDriver<>(url, caps);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-            // Handle the exception as needed, e.g., fail the test or log an error
         }
     }
 
     @Test
     public void sampleTest() {
         System.out.println("I am inside sample test");
-        // Your test logic here
+        driver.get("http://www.google.com");
     }
 
     @AfterTest
